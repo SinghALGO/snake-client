3c18b1ff9f6d620d0354e523752e6629cacc3715
SinghALGO <karanssandhu92@gmail.com>
2023-11-27 13:23:00 -0800

    Added constants file

--- input.js
+++ input.js
@@ -1,4 +1,14 @@
-const setupInput = function () {
+let connection;
+const {
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGTH_KEY,
+  msgKey,
+} = require("./constants");
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,9 +17,19 @@ const setupInput = function () {
 
   return stdin;
 };
-const handleUserInput = function () {
+const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  } else if (key === MOVE_UP_KEY) {
+    connection.write("Move: up");
+  } else if (key === MOVE_LEFT_KEY) {
+    connection.write("Move: left");
+  } else if (key === MOVE_DOWN_KEY) {
+    connection.write("Move: down");
+  } else if (key === MOVE_RIGTH_KEY) {
+    connection.write("Move: right");
+  } else if (key === Object.keys(msgKey)[0]) {
+    connection.write(`Say: ${msgKey[Object.keys(msgKey)[0]]}`);
   }
 };
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,6 +2,5 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
-
-setupInput();
+let x = connect();
+setupInput(x);
